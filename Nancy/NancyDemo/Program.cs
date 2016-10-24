1d9b6661183d5d12965ee853147c016f5f6d6e42
qwegaobinbin <471922845@qq.com>
2016-10-24 13:22:37 +0800

    'tijiao'

---
+++ Nancy/NancyDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NancyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //修改 one
+            using (var host = new NancyHost(new Uri("http://localhost:9001")))
+            {
+                host.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
